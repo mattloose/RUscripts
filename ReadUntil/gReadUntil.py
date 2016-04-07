b0deb71a337782ee402cf1ca9837ed3b0f6d4e1d
Matt Loose <matt.loose@nottingham.ac.uk>
2016-04-07 17:07:57 +0100

    Fixes to speed on gReadUntil.py

--- ReadUntil/gReadUntil.py
+++ ReadUntil/gReadUntil.py
@@ -19,10 +19,12 @@ import configargparse
 import shutil
 import pickle
 import multiprocessing
+import ctypes
 import subprocess
 import re
 import logging
 import platform
+
 
 from ruutils import process_model_file,query_yes_no,send_message
 
@@ -37,15 +39,20 @@ def get_seq_len(ref_fasta):
 
 #######################################################################
 
-def process_ref_fasta(ref_fasta,model_kmer_means,kmer_len):
+def process_ref_fasta(ref_fasta,model_kmer_means,kmer_len,manager):
     print "processing the reference fasta."
     kmer_means=dict()
+    #kmer_array=[]
     for record in SeqIO.parse(ref_fasta, 'fasta'):
+        #Note local dictionary built to force reassignment back to manger dict otherwise it doesn't build correctly
         kmer_means[record.id]=dict()
-        kmer_means[record.id]["F"]=list()
-        kmer_means[record.id]["R"]=list()
-        kmer_means[record.id]["Fprime"]=list()
-        kmer_means[record.id]["Rprime"]=list()
+        tmp=dict()
+        tmp["F"]=list()
+        tmp["R"]=list()
+        tmp["Fprime"]=list()
+        tmp["R"]=list()
+
+
         print "ID", record.id
         print "length", len(record.seq)
         print "FORWARD STRAND"
@@ -53,7 +60,7 @@ def process_ref_fasta(ref_fasta,model_kmer_means,kmer_len):
         seq = record.seq
         for x in range(len(seq)+1-kmer_len):
             kmer = str(seq[x:x+kmer_len])
-            kmer_means[record.id]["F"].append(float(model_kmer_means[kmer]))
+            tmp["F"].append(float(model_kmer_means[kmer]))
             #if model_kmer_means[kmer]:
                 #print x, kmer, model_kmer_means[kmer]
 
@@ -61,34 +68,88 @@ def process_ref_fasta(ref_fasta,model_kmer_means,kmer_len):
         seq = revcomp = record.seq.reverse_complement()
         for x in range(len(seq)+1-kmer_len):
             kmer = str(seq[x:x+kmer_len])
-            kmer_means[record.id]["R"].append(float(model_kmer_means[kmer]))
+            tmp["R"].append(np.float32(model_kmer_means[kmer]))
 
-        kmer_means[record.id]["Fprime"]=sklearn.preprocessing.scale(kmer_means[record.id]["F"], axis=0, with_mean=True, with_std=True, copy=True)
-        kmer_means[record.id]["Rprime"]=sklearn.preprocessing.scale(kmer_means[record.id]["R"], axis=0, with_mean=True, with_std=True, copy=True)
-    return kmer_means
+        tmp["Fprime"]=sklearn.preprocessing.scale(tmp["F"], axis=0, with_mean=True, with_std=True, copy=True)
+        tmp["Rprime"]=sklearn.preprocessing.scale(tmp["R"], axis=0, with_mean=True, with_std=True, copy=True)
+        kmer_means[record.id]=tmp
+        #print "new",kmer_means
+        #sys.exit()
+    #From this dictionary we will return a pair consisting of a list of keys(lookup for sequence name) and a 3D array each slice of which relates to the seqid,forward and reverse and then the values. This will then be used as a numpy shared memory multiprocessing array. We hope.
+
+    items=kmer_means.items()
+    items_=map(processItems,items)
+    #print items_[0][1].shape()
+    #print items_
+    seqids,arrays=zip(*items_)
+    #arrays = [arr for _,arr in items_]
+    #print arrays[0]
+    z=len(seqids)
+    r,c=list(arrays)[0].shape
+    #print z,r,c
+    #print seqids
+    #print map(len,arrays)
+    threedarray=multiprocessing.Array(ctypes.c_double,z*r*c)
+    threedarrayshared_array = np.ctypeslib.as_array(threedarray.get_obj())
+    #print threedarrayshared_array.shape
+    #threedarrayshared_array = threedarrayshared_array.reshape(z*r*c)
+    a = np.array(arrays,dtype=np.float32)
+    #print a.shape
+    threedarrayshared_array = a
+
+    #print threedarrayshared_array.shape
+    #sys.exit()
+    return seqids,threedarrayshared_array
+
+
+def processItems((seqid,d)):
+    result=[]
+    for _,l in d.items():
+        result.append(l)
+    return seqid,np.array(result)
 
 
 #######################################################################
-def squiggle_search2(squiggle,channel_id,read_id,kmerhash,seqlen,args):
+#def squiggle_search2(squiggle,channel_id,read_id,kmerhash,seqlen,args):
+def squiggle_search2(squiggle,channel_id,read_id,args,seqids,threedarray,seqlen):
     result=[]
-
-    for ref in kmerhash:
-        #print "ss2",ref
+    blocksize=200000
+    overlap=blocksize-500
+    for ref in seqids:
+        refid=seqids.index(ref)
+        #print "ss2",ref,refid,threedarray[refid].shape
+        F,R,Fprime,Rprime=threedarray[refid]
+        #sys.exit()
+        #print "squigglesearch"
         queryarray = sklearn.preprocessing.scale(np.array(squiggle),axis=0,with_mean=True,with_std=True,copy=True)
+        refsubset = Fprime
+        #print type(refsubset),refsubset.shape
+        indexes = np.array(xrange(len(refsubset)))
+        subrefs = [refsubset[i:i+blocksize]for i in indexes[::overlap]]
+        for blockid,ref_ in enumerate(subrefs):
+            current = multiprocessing.current_process()
+        #    print "Process is",current,
+        #    print "i",i,
+            #print "ref_",ref_
+            dist, cost, path = mlpy.dtw_subsequence(queryarray,ref_)
+            result.append((dist,ref,"F",path[1][0]+(blockid*overlap),ref))
+        refsubset = Rprime
+        #indexes = np.array(xrange(len(refsubset)))
+        subrefs = [refsubset[i:i+blocksize]for i in indexes[::overlap]]
+        for blockid,ref_ in enumerate(subrefs):
+            #print "i",i,
+            dist, cost, path = mlpy.dtw_subsequence(queryarray,ref_)
+            result.append((dist,ref,"R",path[1][0]+(blockid*overlap),ref))
+        #print "alldone"
 
-        dist, cost, path = mlpy.dtw_subsequence(queryarray,kmerhash[ref]['Fprime'])
-        result.append((dist,ref,"F",path[1][0],ref))
-        dist, cost, path = mlpy.dtw_subsequence(queryarray,kmerhash[ref]['Rprime'])
-        result.append((dist,ref,"R",path[1][0],ref))
-
-
+    #print result
     return sorted(result,key=lambda result: result[0])[0][1],sorted(result,key=lambda result: result[0])[0][0],sorted(result,key=lambda result: result[0])[0][2],sorted(result,key=lambda result: result[0])[0][3],sorted(result,key=lambda result: result[0])[0][4]
 
 ######################################################################
 def extractsquig(events):
     squiggle=list()
     for event in events:
-        squiggle.append(event.mean)
+        squiggle.append(np.float32(event.mean))
     return(squiggle)
 
 class LockedDict(dict):
@@ -136,7 +197,11 @@ def go_or_no(seqid,direction,position,seqlen,args):
     return "Skip"
 
 ###################
-def mp_worker((channel_id, data,kmerhash,seqlen,readstarttime,args)):
+#def mp_worker((channel_id, data,kmerhash,seqlen,readstarttime,args)):
+def mp_worker((channel_id, data,readstarttime,args,seqlen,seqids,threedarray)):
+    #print "mpworker"
+    #current = multiprocessing.current_process()
+    #print "Process is",current.name,"running on",channel_id
     if args.verbose is True:
         print "worker running"
         print "channel_id:",channel_id
@@ -164,8 +229,8 @@ def mp_worker((channel_id, data,kmerhash,seqlen,readstarttime,args)):
     if args.verbose is True:
         print "read number is", readnumber
     if ((time.time()-readstarttime) > args.time):
-        if args.verbose is True:
-            print "We have a timeout"
+        #if args.verbose is True:
+        print "We have a timeout ",channel_id
         #logging.info('%s,%s,%s,%s', channel_id, data.read_id, 'TOT',data.events[0].start)
         return 'timeout',channel_id,readnumber,data.events[0].start
     elif (args.skip is True and int(channel_id) %2 == 0) :
@@ -179,10 +244,13 @@ def mp_worker((channel_id, data,kmerhash,seqlen,readstarttime,args)):
             squiggle = extractsquig(data.events)
             if args.verbose is True:
                 print squiggle
-            squiggleres = squiggle_search2(squiggle,channel_id,readnumber,kmerhash,seqlen,args)
+            #return "skip",channel_id,readnumber,data.events[0].start,"squiggleres"
+            #squiggleres = squiggle_search2(squiggle,channel_id,readnumber,kmerhash,seqlen,args)
+            squiggleres = squiggle_search2(squiggle,channel_id,readnumber,args,seqids,threedarray,seqlen)
             if args.verbose is True: print squiggleres
             result = go_or_no(squiggleres[0],squiggleres[2],squiggleres[3],seqlen,args)
             if args.verbose is True: print "Result ",result
+            #print result,channel_id,readnumber,data.events[0].start,squiggleres
             return result,channel_id,readnumber,data.events[0].start,squiggleres
         except Exception, err:
             err_string="Time Warping Stuff : %s" % ( err)
@@ -194,11 +262,11 @@ def mp_worker((channel_id, data,kmerhash,seqlen,readstarttime,args)):
 
 
 
-def run_analysis():
-    analyser = MyAnalyser()
+def run_analysis(args,analyser):
+    #analyser = MyAnalyser(args)
     host = "ws://"+str(args.ip)+":"+str(args.port)+"/"
-    setup_conditions = {"ignore_first_events": 100, "padding_length_events": 0,
-                        "events_length": 250, "repetitions": 1}
+    setup_conditions = {"ignore_first_events": 75, "padding_length_events": 0,
+                        "events_length": 128, "repetitions": 1}
 
     state=RunningState()
     with ReadUntil(host=host,
@@ -211,7 +279,7 @@ def run_analysis():
             print err
         print "Client connection started. Beginning unblock loop..."
         while state.keep_running:
-            #print "looping"
+            print "looping"
             unblock_now = analyser.next_unblock_map()
             if len(unblock_now)>0:
                 if args.verbose is True: print "Unblocking channels: ", unblock_now.keys()
@@ -224,6 +292,90 @@ def run_analysis():
             # and on the hardware to unblock a bigger list of channels at once.
             time.sleep(1)
         print "...unblock loop ended. Connection closed."
+
+class MyAnalyser:
+    """
+    Analyses recent data for each channel and amends the list of the channel
+    ids that should be unblocked. ReadUntil should call data_received whenever
+    some new data is available. It may call it several times concurrently and
+    possibly with updates to the same channel if in the setup conditions we
+    have requested more than 1 update for a given read.  We react by keeping up
+    to date a current_unblock_set. Then we regularly make a separate unblock
+    call using that list in another thread.
+
+    This demonstrates one way of arranging code to analyse and unblock.
+    How best to handle concurrent overlapping updates and when to unblock is an
+    open problem left to the client script to resolve which will depend on what
+    it is trying to achieve.
+    """
+    def __init__(self,args,p,seqids,threedarray):
+        #print "class keys", kmerhash.keys()
+        #print "hello im the class"
+
+        #self.kmerhash=kmerhash
+        #self.seqlen=seqlen
+        #self.p = multiprocessing.Pool(args.procs)
+
+        self.current_unblock_map = LockedDict()
+        #self.p = multiprocessing.Pool(48)
+        #for id in kmerhash:
+        #for ref in kmerhash[id]:
+        #    print id,ref
+
+    def mycallback(self, actions):
+        if actions[0] == "Skip":
+            if args.verbose is True: print "Should skip"
+            self.current_unblock_map[actions[1]]=actions[2]
+            logging.info('%s,%s,%s,%s,%s,%s,%s,%s', actions[1], actions[2], 'REJ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3])
+        elif actions[0] == "timeout":
+            if args.verbose is True: print "Read timeout"
+            logging.info('%s,%s,%s,%s', actions[1], actions[2], 'TOT',actions[3])
+            #logging.info('%s,%s,%s', actions[1],actions[2],"TOT")
+        elif actions[0] == "evenskip":
+            if args.verbose is True: print "Even Skip"
+            logging.info('%s,%s,%s,%s', actions[1], actions[2], 'EVE',actions[3])
+        else:
+            if args.verbose is True: print "Sequencing from ",action[1],action[2]
+            #print actions[1], actions[2], 'SEQ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3]
+            logging.info('%s,%s,%s,%s,%s,%s,%s,%s', actions[1], actions[2], 'SEQ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3])
+
+
+    def apply_async_with_callback(self, channels):
+        #print "async called", kmerhash.keys()
+        if args.verbose is True: print "Channels length",len(channels)
+        d=list()
+        if args.verbose is True: print "Checking Channels"
+        for channel_id, data in channels.iteritems():
+            #print channel_id
+            #self.p.apply_async(mp_worker, args = ((channel_id,data,self.kmerhash,self.seqlen,time.time(),args), ), callback = self.mycallback)
+            #self.p.apply_async(mp_worker, args = ((channel_id,data,time.time(),args), ), callback = self.mycallback)
+            p.apply_async(mp_worker, args = ((channel_id,data,time.time(),args,seqlen,seqids,threedarray), ), callback = self.mycallback)
+
+
+    def next_unblock_map(self):
+       """
+       Returns current map of channel_id to read_id that should be unblocked but
+       also clears the map in the assumption that the action to unblock will be
+       carried out straightaway.
+       """
+       return self.current_unblock_map.pop_all_and_clear()
+
+class RunningState:
+    def __init__(self):
+        self.keep_running=True
+
+    def closed(self, *args):
+        self.keep_running=False
+
+def initializer(kmerhash):
+    global poolkmerhash
+    poolkmerhash=dict()
+    print "kmerhash",type(kmerhash)
+    poolkmerhash=kmerhash
+    print "pool",type(poolkmerhash)
+    print eval(poolkmerhash)
+    #sys.exit()
+
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
@@ -245,8 +397,8 @@ if __name__ == "__main__":
     if (oper is "windows"):
             config_file = os.path.join(os.path.sep, os.path.dirname(os.path.realpath('__file__')), 'ampW.config')
 
-    __version__ = "1.0"
-    __date__ = "30th March 2016"
+    __version__ = "1.1"
+    __date__ = "7th April 2016"
 
     parser = configargparse.ArgParser(description='gReadUntil.py: A program providing read until for genome sequences with the Oxford Nanopore minION device. This program will ultimately be driven by minoTour to enable selective remote sequencing. This program is partly based on original code generously provided by Oxford Nanopore Technologies.')
     parser.add('-fasta', '--reference_fasta_file', type=str, dest='fasta', required=True, default=None, help="The fasta format file describing the reference sequence for your organism.")
@@ -265,85 +417,28 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
 
-    class MyAnalyser:
-        """
-        Analyses recent data for each channel and amends the list of the channel
-        ids that should be unblocked. ReadUntil should call data_received whenever
-        some new data is available. It may call it several times concurrently and
-        possibly with updates to the same channel if in the setup conditions we
-        have requested more than 1 update for a given read.  We react by keeping up
-        to date a current_unblock_set. Then we regularly make a separate unblock
-        call using that list in another thread.
+    fasta_file = args.fasta
+    #global seqlen
+    seqlen = get_seq_len(fasta_file)
+    #print type(seqlen)
+    print seqlen
+    model_file = args.temp_model
+    global model_kmer_means
+    global kmer_len
+    model_kmer_means,kmer_len=process_model_file(model_file)
+    #model_kmer_means = retrieve_model()
+    #global kmerhash
 
-        This demonstrates one way of arranging code to analyse and unblock.
-        How best to handle concurrent overlapping updates and when to unblock is an
-        open problem left to the client script to resolve which will depend on what
-        it is trying to achieve.
-        """
+    #kmerhash = manager.dict()
+    #kmerhash = process_ref_fasta(fasta_file,model_kmer_means,kmer_len,manager)
+    seqids,threedarray = process_ref_fasta(fasta_file,model_kmer_means,kmer_len,manager)
+    #print "init kmerhash",type(kmerhash)
 
-        p = multiprocessing.Pool(args.procs)
-        fasta_file = args.fasta
-        seqlen = get_seq_len(fasta_file)
-        #print type(seqlen)
-        print seqlen
-        model_file = args.temp_model
-        model_kmer_means,kmer_len=process_model_file(model_file)
-        #model_kmer_means = retrieve_model()
-        #global kmerhash
-        kmerhash = process_ref_fasta(fasta_file,model_kmer_means,kmer_len)
-
-        print type (kmerhash)
+    print type(threedarray)
 
 
-        def __init__(self):
-            self.current_unblock_map = LockedDict()
-            #self.p = multiprocessing.Pool(48)
-            #for id in kmerhash:
-            #for ref in kmerhash[id]:
-            #    print id,ref
-
-        def mycallback(self, actions):
-            if actions[0] == "Skip":
-                if args.verbose is True: print "Should skip"
-                self.current_unblock_map[actions[1]]=actions[2]
-                logging.info('%s,%s,%s,%s,%s,%s,%s,%s', actions[1], actions[2], 'REJ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3])
-            elif actions[0] == "timeout":
-                if args.verbose is True: print "Read timeout"
-                logging.info('%s,%s,%s,%s', actions[1], actions[2], 'TOT',actions[3])
-                #logging.info('%s,%s,%s', actions[1],actions[2],"TOT")
-            elif actions[0] == "evenskip":
-                if args.verbose is True: print "Even Skip"
-                logging.info('%s,%s,%s,%s', actions[1], actions[2], 'EVE',actions[3])
-            else:
-                if args.verbose is True: print "Sequencing from ",action[1],action[2]
-                #print actions[1], actions[2], 'SEQ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3]
-                logging.info('%s,%s,%s,%s,%s,%s,%s,%s', actions[1], actions[2], 'SEQ',actions[3],actions[4][0],actions[4][1],actions[4][2],actions[4][3])
-
-
-        def apply_async_with_callback(self, channels):
-            #print "async called"
-            if args.verbose is True: print "Channels length",len(channels)
-            d=list()
-            if args.verbose is True: print "Checking Channels"
-            for channel_id, data in channels.iteritems():
-                #print channel_id
-                MyAnalyser.p.apply_async(mp_worker, args = ((channel_id,data,MyAnalyser.kmerhash,MyAnalyser.seqlen,time.time(),args), ), callback = self.mycallback)
-
-        def next_unblock_map(self):
-           """
-           Returns current map of channel_id to read_id that should be unblocked but
-           also clears the map in the assumption that the action to unblock will be
-           carried out straightaway.
-           """
-           return self.current_unblock_map.pop_all_and_clear()
-
-    class RunningState:
-        def __init__(self):
-            self.keep_running=True
-
-        def closed(self, *args):
-            self.keep_running=False
-
+    p = multiprocessing.Pool(args.procs)
+    analyser = MyAnalyser(args,p,seqids,threedarray)
     messagesend = True
     try:
         if messagesend: send_message("minoTour software is implementing read until on this version of minKNOW and will reject reads being sequenced. If you don\'t want this to happen, you should ensure ws_event_sampler is not running! You proceed at your own risk.",args.ip)
@@ -397,7 +492,7 @@ if __name__ == "__main__":
     while 1:
         try:
             print "Running Analysis"
-            run_analysis()
+            run_analysis(args,analyser)
         except socket_error as serr:
             if serr.errno != errno.ECONNREFUSED:
                 raise serr
